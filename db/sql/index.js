658421b8f4e36fca00a03cef5a618639e99f46e4
kissmygritts <kissmygritts08@gmail.com>
2017-01-20 08:36:20 -0800

    #1 use debug option for QueryFile

--- db/sql/index.js
+++ db/sql/index.js
@@ -5,7 +5,10 @@ const path = require('path');
 
 function sql(file) {
   const fullPath = path.join(__dirname, file);
-  const options = { minify: true };
+  const options = {
+    minify: true,
+    debug: true
+  };
   const qf = new QueryFile(fullPath, options);
 
   if (qf.error) {
