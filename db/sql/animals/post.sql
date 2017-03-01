6970a109111fc7b92e96fd12b609eec99c608495
kissmygritts <kissmygritts08@gmail.com>
2017-03-01 13:32:03 -0800

    untrack database config, add animals post

---
+++ db/sql/animals/post.sql
@@ -0,0 +1,24 @@
+INSERT INTO animals (
+  perm_id,
+  species,
+  serial_num,
+  cap_date,
+  sex,
+  age,
+  study_id,
+  mgmt_area,
+  notes,
+  attributes
+)
+VALUES (
+  ${perm_id},
+  ${species},
+  ${serial_num},
+  ${cap_date},
+  ${sex},
+  ${age},
+  ${study_id},
+  ${mgmt_area},
+  ${notes},
+  ${attributes:json}
+)
